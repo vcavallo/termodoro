97e05a93a5564d603de88945bde7b7ec9ee0dcf3
Vinney Cavallo <vcavallo@gmail.com>
2013-10-24 11:45:56 -0400

    parsing time_part works

--- lib/termodoro.rb
+++ lib/termodoro.rb
@@ -3,32 +3,36 @@ require_relative '../config/environment'
 ###> WIP Class. Procedural below <###
 
 class Termodoro
+  attr_accessor :time_part, :number_part
 
   SECS_IN_MIN = 60
   SECS_IN_HOUR = 3600
 
   def initialize(arguments)
     @arguments = arguments
+
     # message
     # title on/off
   end
 
-  def time_part
+  def parse_time_part
     segment = @arguments.scan(/[\D]+[^\w]/).first.strip
+    self.time_part = segment
     #=> hours/minutes/seconds, etc.
   end
 
-  def number_part
+  def parse_number_part
     number = @arguments.scan(/[\d]+/).first.strip.to_i
+    self.number_part = number
     #=> the number of time_parts given
   end
 
   def calculate_time
-    if time_part.minutes?
+    if minutes?
       seconds = number_part * SECS_IN_MIN
-    elsif time_part.hours?
+    elsif hours?
       seconds = number_part * SECS_IN_HOUR
-    elsif time_part.seconds?
+    elsif seconds?
       seconds = number_part
     end
   
@@ -43,19 +47,19 @@ class Termodoro
 
   def seconds?
     seconds = %w[s sec secs second seconds]
-    true if seconds.include?(self.time_part)
+    true if seconds.include?(parse_time_part)
     #=> true/false
   end
 
   def minutes?
     minutes = %w[m min mins minute minutes]
-    true if minutes.include?(self.time_part)
+    true if minutes.include?(parse_time_part)
     #=> true/false
   end
 
   def hours?
     hours = %w[h hr hrs hour hours]
-    true if hours.include?(self.time_part)
+    true if hours.include?(parse_time_part)
     #=> true/false
   end
 
