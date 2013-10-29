d3800365095e57b66d1db83d70a05e75c6ec3875
Vinney Cavallo <vcavallo@gmail.com>
2013-10-29 19:49:29 -0400

    not sure what changed here yet. need to integrate scott's changes

--- lib/termodoro.rb
+++ lib/termodoro.rb
@@ -12,6 +12,12 @@ class Termodoro
     # title on/off -- probably going to drop this
   end
 
+  # parse class
+    # Parse.new(input)
+      # 
+  # calculate class
+  # run clean_message
+
   def parse_time_part
     segment = @arguments.scan(/[\D]+[^\w]/).first.split(" ").first
     self.time_part = segment
